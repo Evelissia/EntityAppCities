506f54267b74fb50d85a1a73c576600438c3bd91
Evelissia <evelissia@gmail.com>
2022-05-23 21:05:42 +0600

    MVC веб приложение с картой, на которую выгружаются все города

--- CitiesApp/Controllers/CitiesController.cs
+++ CitiesApp/Controllers/CitiesController.cs
@@ -1,6 +1,7 @@
 ﻿using CitiesApp.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Net;
 
 namespace CitiesApp.Controllers
 {
@@ -14,6 +15,32 @@ namespace CitiesApp.Controllers
         {
             this._context = context;
         }
+
+        /*[HttpGet("parse")]
+        public ActionResult<City> Post()
+        {
+            string URL = "https://raw.githubusercontent.com/pensnarik/russian-cities/master/russian-cities.json";
+            WebRequest request = WebRequest.Create(URL);
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            WebResponse response = request.GetResponse();
+            // Display the status.
+            using (Stream dataStream = response.GetResponseStream())
+            {
+                // Open the stream using a StreamReader for easy access.
+                StreamReader reader = new StreamReader(dataStream);
+                // Read the content.
+                string responseFromServer = @reader.ReadToEnd();
+
+                List<City> city = Newtonsoft.Json.JsonConvert.DeserializeObject<List<City>>(responseFromServer);
+                _context.AddRange(city);
+                _context.SaveChanges();
+            }
+
+            // Close the response.
+            response.Close();
+            return Ok();
+        }*/
 
         [HttpGet()]
         public List<City> GetCities()
